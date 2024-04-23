c159da4c1b101fba8f14d8c3dea4d4e5ca8e265a
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-04-23 13:46:24 +0700

    add vue and config

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Homeview from './views/Homeview.vue';
+</script>
+
+<template>
+  <Homeview />
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {}
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#06b6d4",
+          secondary: "#666666",
+          accent: "#111827",
+          neutral: "#44403c",
+          "base-100": "#333333",
+          info: "#60a5fa",
+          success: "#16a34a",
+          warning: "#facc15",
+          error: "#ef4444"
+        }
+      }
+    ]
+  },
+  plugins: [require("daisyui")]
+}
