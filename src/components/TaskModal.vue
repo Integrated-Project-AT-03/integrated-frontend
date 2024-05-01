1dbf1031cc63056b340a58905bf62f947fdc8457
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-05-01 17:51:30 +0700

    add: editItem

--- src/components/TaskModal.vue
+++ src/components/TaskModal.vue
@@ -1,24 +1,50 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref, watch, } from 'vue';
+import { editItem } from "../assets/fetch.js";
+import { useRouter } from "vue-router";
 
-defineProps({
+const router = useRouter();
+const props = defineProps({
   dataModal: {
     type: Object,
   }
 });
-const formModal = ref()
+const uri = import.meta.env.VITE_SERVER_URI;
+
+
+const formModal = ref(props.dataModal)
+watch(() => props.dataModal,()=>
+  formModal.value = props.dataModal
+)
+async function editTask(id){
+  try {
+    const edit = await editItem(`${uri}/v1/tasks`,id,formModal)
+    router.push({ path: `/task` })
+    if(edit.status === 404){
+      console.log('Error404');
+    }
+  } catch (error) {
+  }
+} 
+
 const localZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 const formattDate = (date) =>
+
   new Date(date).toLocaleString("en-GB", localZone).replace(",", "");
 </script>
 
+
 <template>
+
+
+
   <dialog
     id="showDetail"
     class="w-[65rem] m-auto h-[47rem] bg-neutral rounded-2xl"
   >
+
     <div class="itbkk-title text-xl text-slate-200 mt-5 ml-6 font-bold">
-      <input class="bg-neutral hover:border-neutral" type="text" :value=" dataModal?.title" />
+      <input class="bg-neutral hover:border-neutral" type="text" :value=" formModal?.title" />
     </div>
     <div class="divider"></div>
     <div class="flex justify-around m-4">
@@ -27,13 +53,13 @@ const formattDate = (date) =>
         <textarea
           class="itbkk-description w-[35rem] h-[32rem] rounded-2xl border p-4 bg-secondary border-base-100"
           :class="
-            dataModal?.description !== ''
+            formModal?.description !== ''
               ? ' text-slate-200'
               : 'text-gray-400 italic'
           "
           >{{
-            dataModal?.description !== ""
-              ? dataModal.description
+            formModal?.description !== ""
+              ? formModal.description
               : "No Description Provided"
           }}</textarea
         >
@@ -44,12 +70,12 @@ const formattDate = (date) =>
           <textarea
             class="itbkk-assignees w-[20rem] h-[12rem] rounded-2xl border p-4 bg-secondary border-base-100"
             :class="
-              dataModal?.assignees !== ''
+              formModal?.assignees !== ''
                 ? ' text-slate-200'
                 : 'text-gray-400 italic'
             "
             >{{
-              dataModal?.assignees !== "" ? dataModal.assignees : "Unassigned"
+              formModal?.assignees !== "" ? formModal.assignees : "Unassigned"
             }}</textarea
           >
         </div>
@@ -59,9 +85,9 @@ const formattDate = (date) =>
             <option selected >
               No status
             </option>
-            <option :selected="dataModal?.status === 'doing'">Doing</option>
-            <option :selected="dataModal?.status === 'done'">Done</option>
-            <option :selected="dataModal?.status === 'to_do'">To do</option>
+            <option :selected="formModal?.status === 'doing'">Doing</option>
+            <option :selected="formModal?.status === 'done'">Done</option>
+            <option :selected="formModal?.status === 'to_do'">To do</option>
           </select>
         </div>
         <div class="flex flex-col h-3/4 gap-3 text-slate-200">
@@ -74,13 +100,13 @@ const formattDate = (date) =>
           <div class="flex gap-2">
             Created On:
             <div class="itbkk-created-on">
-              {{ formattDate(dataModal?.createdOn) }}
+              {{ formattDate(formModal?.createdOn) }}
             </div>
           </div>
           <div class="flex gap-2">
             Updated On:
             <div class="itbkk-updated-on">
-              {{ formattDate(dataModal?.updatedOn) }}
+              {{ formattDate(formModal?.updatedOn) }}
             </div>
           </div>
         </div>
@@ -88,7 +114,7 @@ const formattDate = (date) =>
     </div>
     <div class="divider"></div>
     <div class="flex justify-end m-4 gap-3">
-      <button
+      <button @click="editTask(dataModal.id)"
         class="btn btn-success w-16 hover:bg-base-100 hover:border-base-100"
       >
         Ok
