0d37c6bfc0f392a5df61a9000e93a973a7fa77cd
realFK98 <wishira2013.coth@gmail.com>
2024-05-07 10:36:37 +0700

    add cypress script and fix fetch uri

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://ip23nw2.sit.kmutt.ac.th:5173",
+    baseUrl: "http://ip23at3.sit.kmutt.ac.th",
   },
   component: {
     specPattern: "src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
