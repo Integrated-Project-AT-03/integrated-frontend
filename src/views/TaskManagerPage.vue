5c70fce902e7e945166a980452fd686772e3b9f7
Lattawit071 <phuri.chai@mail.kmutt.ac.th>
2024-05-15 15:43:00 +0700

    Status

--- src/views/TaskManagerPage.vue
+++ src/views/TaskManagerPage.vue
@@ -4,6 +4,7 @@ import TaskManagement from "./../lib/TaskManagement.js";
 import Loading from "./../components/Loading.vue";
 import { useRoute, useRouter } from "vue-router";
 import { getItems } from "./../assets/fetch.js";
+import StatusModal from "@/components/StatusModal.vue";
 
 const datas = ref(TaskManagement);
 const uri = import.meta.env.VITE_SERVER_URI;
@@ -25,32 +26,20 @@ const handleMessage = (e) => {
 
 <template>
   <Loading :is-loading="isLoading" />
-  <div
-    class="container mx-auto flex flex-col gap-3"
-    :class="
-      route.fullPath.split('/').length > 2 ||
-      (route.name === 'Addstatus' && 'blur-sm')
-    "
-  >
+  <div class="container mx-auto flex flex-col gap-3" :class="route.fullPath.split('/').length > 2 ||
+    (route.name === 'Addstatus' && 'blur-sm')
+    ">
     <div class="text-5xl font-extrabold ... w-full flex justify-center m-7">
-      <span
-        class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"
-      >
+      <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
         <div class="text-5xl">IT-Bangmod Kradan Kanban</div>
       </span>
     </div>
     <div class="w-full flex items-center justify-end">
       <div class="flex justify-end gap-4">
-        <button
-          class="itbkk-manage-status btn btn-secondary"
-          @click="router.push({ name: 'Statuses' })"
-        >
+        <button class="itbkk-manage-status btn btn-secondary" @click="router.push({ name: 'Statuses' })">
           Manage Status
         </button>
-        <button
-          @click="$router.push({ name: 'AddTask' })"
-          class="itbkk-button-add btn btn-primary text-slate-300"
-        >
+        <button @click="$router.push({ name: 'AddTask' })" class="itbkk-button-add btn btn-primary text-slate-300">
           + Add task
         </button>
       </div>
@@ -58,28 +47,16 @@ const handleMessage = (e) => {
     <table class="min-w-full divide-y divide-gray-200">
       <thead class="bg-gray-200">
         <tr>
-          <th
-            scope="col"
-            class="px-6 py-3 text-left text-sm font-bold text-base-100 uppercase tracking-wider"
-          >
+          <th scope="col" class="px-6 py-3 text-left text-sm font-bold text-base-100 uppercase tracking-wider">
             ID
           </th>
-          <th
-            scope="col"
-            class="px-6 py-3 text-left text-sm font-bold text-base-100 uppercase tracking-wider"
-          >
+          <th scope="col" class="px-6 py-3 text-left text-sm font-bold text-base-100 uppercase tracking-wider">
             Title
           </th>
-          <th
-            scope="col"
-            class="px-6 py-3 text-left text-sm font-bold text-base-100 uppercase tracking-wider"
-          >
+          <th scope="col" class="px-6 py-3 text-left text-sm font-bold text-base-100 uppercase tracking-wider">
             Assignees
           </th>
-          <th
-            scope="col"
-            class="px-6 py-3 text-left text-sm font-bold text-base-100 uppercase tracking-wider"
-          >
+          <th scope="col" class="px-6 py-3 text-left text-sm font-bold text-base-100 uppercase tracking-wider">
             Status
           </th>
         </tr>
@@ -90,12 +67,8 @@ const handleMessage = (e) => {
             No task
           </td>
         </tr>
-        <tr
-          class="itbkk-item itbkk-button-action hover:bg-slate-200"
-          v-for="(task, index) in datas.getTasks()"
-          :key="task.id"
-          @click="$router.push({ name: 'TaskDetail', params: { id: task.id } })"
-        >
+        <tr class="itbkk-item itbkk-button-action hover:bg-slate-200" v-for="(task, index) in datas.getTasks()"
+          :key="task.id" @click="$router.push({ name: 'TaskDetail', params: { id: task.id } })">
           <td class="px-6 py-4 whitespace-nowrap">
             <div class="text-sm text-gray-900">{{ index.id }}</div>
           </td>
@@ -103,24 +76,15 @@ const handleMessage = (e) => {
             <div class="text-sm text-gray-900">{{ task.title }}</div>
           </td>
           <td class="px-6 py-4 whitespace-nowrap">
-            <div
-              class="text-sm text-gray-900 itbkk-assignees"
-              :class="task?.assignees ?? 'italic'"
-            >
+            <div class="text-sm text-gray-900 itbkk-assignees" :class="task?.assignees ?? 'italic'">
               {{ task?.assignees ?? "Unassigned" }}
             </div>
+
           </td>
           <td class="itbkk-status px-6 py-4 whitespace-nowrap">
-            <div
-              class="status flex justify-center text-sm w-20 p-2 rounded-xl text-slate-200"
-              :style="`background-color: ${task.statusColor}`"
-            >
-              {{
-                task.status.length > 10
-                  ? task.status.slice(0, 7) + "..."
-                  : task.status
-              }}
-            </div>
+
+            <StatusModal :status-color="task.statusColor" :text="task.status" />
+
           </td>
         </tr>
       </tbody>
