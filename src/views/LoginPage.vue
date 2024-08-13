a2b38f47cd53122a49dcc3dc0bc034073244a312
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-08-13 16:10:12 +0700

    plan-for-fetch-user

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,21 +1,28 @@
 <script setup>
 import { computed, ref } from 'vue';
 import Button from '../components/ButtonModal.vue'
+import { addItem } from '@/lib/fetch';
 
+const uri = import.meta.env.VITE_SERVER_URI
 const user = ref({
-  username: '',
+  userName: '',
   password: ''
 })
 
 const validateInput = computed(() => {
   return {
-    username: user.value.username.length > 50,
+    username: user.value.userName.length > 50,
     password: user.value.password.length > 14
   };
 });
 
 async function login(data) {
-  
+    try {
+        const res = await addItem(`${uri}/Endpoint`, data)
+        return res
+    } catch (error) {
+        return error
+    }
 }
 
 </script>
@@ -27,7 +34,7 @@ async function login(data) {
       <div class="flex flex-col gap-3 items-center w-full">
         <div class="flex flex-col gap-2 text-xs">
             {{ validateInput.username ? 'Max 50 character' : '' }}
-            <input v-model="user.username" type="text" placeholder="Username" class="itbkk-username input input-bordered w-[20rem] rounded-xl max-w-xs" />
+            <input v-model="user.userName" type="text" placeholder="Username" class="itbkk-username input input-bordered w-[20rem] rounded-xl max-w-xs" />
         </div>
         <div class="flex flex-col gap-2 text-xs">
             {{ validateInput.password ? 'Max 14 character' : '' }}
@@ -41,10 +48,11 @@ async function login(data) {
                 message="Sign up" />
             </div>
             <div class="">
-                <Button :disabled="user.username.length <= 0 || user.password.length <= 0"
+                <Button :disabled="user.userName.length <= 0 || user.password.length <= 0"
                 class="itbkk-button-signin w-[10rem] rounded-[1.4rem]"
                 bgcolor="#666666"
-                message="Sign in" />
+                message="Sign in" 
+                @click="login(user)" />
             </div>
         </div>
       </div>
