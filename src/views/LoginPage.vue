d0525151132c7cc7f67ae0a4232c9732229e39f3
realFK98 <wishira2013.coth@gmail.com>
2024-08-24 10:12:50 +0700

    fix endpoint

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,82 +1,96 @@
 <script setup>
-import { computed, ref } from 'vue';
-import Button from '../components/ButtonModal.vue';
-import { addItem } from '@/lib/fetch';
-import { useRouter } from 'vue-router';
+import { computed, ref } from "vue";
+import Button from "../components/ButtonModal.vue";
+import { addItem } from "@/lib/fetch";
+import { useRouter } from "vue-router";
 
-const router = useRouter()
+const router = useRouter();
 const uri = import.meta.env.VITE_SERVER_URI;
 
-const user = ref({userName: '' ,password: ''})
-const errorMessage = ref()
+const user = ref({ userName: "", password: "" });
+const errorMessage = ref();
 
-
-async function onSubmit(item){
-  errorMessage.value = ''
+async function onSubmit(item) {
+  errorMessage.value = "";
 
   if (!user.value.userName || !user.value.password) {
-    errorMessage.value = 'Did not enter a username or password.';
+    errorMessage.value = "Did not enter a username or password.";
     return;
   }
 
-  const res = await addItem(`${uri}/authentications/login`, item)
+  const res = await addItem(`${uri}/login`, item);
   if (res.httpStatus === 200) {
-    const parseJson = JSON.stringify(res.access_token)
-    localStorage.setItem("token", parseJson)
-    router.push("/app")
+    const parseJson = JSON.stringify(res.access_token);
+    localStorage.setItem("token", parseJson);
+    router.push("/app");
   } else {
-    errorMessage.value = 'Username or Password is incorrect';
+    errorMessage.value = "Username or Password is incorrect";
   }
 }
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col justify-center items-center">
-    <div class="w-[24rem] p-8  rounded-lg">
-
+  <div class="flex min-h-screen flex-col items-center justify-center">
+    <div class="w-[24rem] rounded-lg p-8">
       <h2
-        class="text-3xl font-bold bg-gradient-to-r from-[#CE50B7] via-[#BF53C5] to-[#EA499A] bg-clip-text text-transparent mb-2">
-        Welcome back </h2>
-      <p class="text-lg text-gray-400 mb-8">
-        welcome to <span
-          class="bg-gradient-to-r from-[#CE50B7] via-[#BF53C5] to-[#EA499A] bg-clip-text text-transparent">IT-BKK</span>
+        class="mb-2 bg-gradient-to-r from-[#CE50B7] via-[#BF53C5] to-[#EA499A] bg-clip-text text-3xl font-bold text-transparent"
+      >
+        Welcome back
+      </h2>
+      <p class="mb-8 text-lg text-gray-400">
+        welcome to
+        <span
+          class="bg-gradient-to-r from-[#CE50B7] via-[#BF53C5] to-[#EA499A] bg-clip-text text-transparent"
+          >IT-BKK</span
+        >
       </p>
 
-
       <div class="mb-4">
-        <label for="email" class="block text-gray-400 mb-2">userName</label>
-        <input type="email" id="email" placeholder="Enter your email" v-model="user.userName"
-          class="itbkk-username w-full px-3 py-2 bg-gray-700 text-gray-300 border border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-[#BF53C5]" 
-          maxlength="50"/>
+        <label for="email" class="mb-2 block text-gray-400">userName</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+          v-model="user.userName"
+          class="itbkk-username w-full rounded border border-gray-600 bg-gray-700 px-3 py-2 text-gray-300 focus:outline-none focus:ring-2 focus:ring-[#BF53C5]"
+          maxlength="50"
+        />
       </div>
 
-
       <div class="mb-4">
-        <label for="password" class="block text-gray-400 mb-2">Password</label>
-        <input type="password" id="password" placeholder="Password" v-model="user.password"
-          class="itbkk-password w-full px-3 py-2 bg-gray-700 text-gray-300 border border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-[#BF53C5]" 
-          maxlength="14"/>
+        <label for="password" class="mb-2 block text-gray-400">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          v-model="user.password"
+          class="itbkk-password w-full rounded border border-gray-600 bg-gray-700 px-3 py-2 text-gray-300 focus:outline-none focus:ring-2 focus:ring-[#BF53C5]"
+          maxlength="14"
+        />
       </div>
 
-
-      <div v-show="errorMessage" class="itbkk-message mb-4 text-red-500 text-sm">
+      <div
+        v-show="errorMessage"
+        class="itbkk-message mb-4 text-sm text-red-500"
+      >
         {{ errorMessage }}
       </div>
 
-      <div class="text-left mb-6">
+      <div class="mb-6 text-left">
         <a href="#" class="text-white-300 hover:underline">Forgot password?</a>
       </div>
 
-
-      <button @click="onSubmit(user)"
-        class="itbkk-button-signin btn w-full py-2 bg-gradient-to-r from-[#CE50B7] via-[#BF53C5] to-[#EA499A]  text-white font-semibold rounded-lg" 
-        :disabled="user.userName.length === 0 || user.password.length ===0">
+      <button
+        @click="onSubmit(user)"
+        class="itbkk-button-signin btn w-full rounded-lg bg-gradient-to-r from-[#CE50B7] via-[#BF53C5] to-[#EA499A] py-2 font-semibold text-white"
+        :disabled="user.userName.length === 0 || user.password.length === 0"
+      >
         Login
       </button>
 
-
-      <div class="text-center mt-6 text-white-100">
-        Don't have an account? <a href="#" class="text-white hover:underline">Sign up for free</a>
+      <div class="text-white-100 mt-6 text-center">
+        Don't have an account?
+        <a href="#" class="text-white hover:underline">Sign up for free</a>
       </div>
     </div>
   </div>
