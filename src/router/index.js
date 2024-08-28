7b011da45e09828ee4464ce4bdf56a47c153d592
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-08-28 22:48:08 +0700

    add-nevigation-guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,74 +1,76 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import EmptyModal from '@/components/EmptyModal.vue';
-import AddTaskModal from '@/components/AddTaskModal.vue';
-import ShowTaskModal from '@/components/ShowTaskModal.vue';
-import StatusManagerPage from './../views/TaskStatusPage.vue';
-import TaskManagerPage from './../views/TaskManagerPage.vue';
-import EditStatusModal from '@/components/EditStatusModal.vue';
-import AddStatusModal from '@/components/AddStatusModal.vue';
-import LoginPage from '../views/LoginPage.vue';
-import AppPage from '@/views/AppPage.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import EmptyModal from "@/components/EmptyModal.vue";
+import AddTaskModal from "@/components/AddTaskModal.vue";
+import ShowTaskModal from "@/components/ShowTaskModal.vue";
+import StatusManagerPage from "./../views/TaskStatusPage.vue";
+import TaskManagerPage from "./../views/TaskManagerPage.vue";
+import EditStatusModal from "@/components/EditStatusModal.vue";
+import AddStatusModal from "@/components/AddStatusModal.vue";
+import LoginPage from "../views/LoginPage.vue";
+import AppPage from "@/views/AppPage.vue";
+import { parseJwt } from "@/utils/helper";
+import { ref } from "vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: '/login',
+      path: "/",
+      redirect: "/login",
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
       component: LoginPage,
     },
     {
-      path: '/app',
-      name: 'App',
+      path: "/app",
+      name: "App",
       component: AppPage,
       children: [
         {
-          path: '/app',
-          redirect: 'app/task',
+          path: "/app",
+          redirect: "app/task",
         },
         {
-          path: 'task',
-          name: 'Task',
+          path: "task",
+          name: "Task",
           component: TaskManagerPage,
           children: [
             {
-              path: ':id',
-              name: 'TaskDetail',
+              path: ":id",
+              name: "TaskDetail",
               component: ShowTaskModal,
               children: [
                 {
-                  path: ':mode',
-                  name: 'TaskEdit',
+                  path: ":mode",
+                  name: "TaskEdit",
                   component: EmptyModal,
                 },
               ],
             },
 
             {
-              path: 'task/add',
-              name: 'AddTask',
+              path: "task/add",
+              name: "AddTask",
               component: AddTaskModal,
             },
           ],
         },
 
         {
-          path: 'status',
-          name: 'Status',
+          path: "status",
+          name: "Status",
           component: StatusManagerPage,
           children: [
             {
-              path: ':id/edit',
-              name: 'EditStatus',
+              path: ":id/edit",
+              name: "EditStatus",
               component: EditStatusModal,
             },
             {
-              path: 'add',
-              name: 'AddStatus',
+              path: "add",
+              name: "AddStatus",
               component: AddStatusModal,
             },
           ],
@@ -76,6 +78,36 @@ const router = createRouter({
       ],
     },
   ],
+});
+
+const jwtPayload = ref();
+
+function isTokenValid(token) {
+  if (!token) {
+    return false;
+  }
+  try {
+    const token = localStorage.getItem("token");
+    jwtPayload.value = parseJwt(token);
+    const currenTime = Date.now() / 1000;
+    return token.exp > currenTime;
+  } catch (error) {
+    return false;
+  }
+}
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("jwt");
+
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (isTokenValid(token)) {
+      next();
+    } else {
+      next("/login");
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
