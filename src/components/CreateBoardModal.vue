477f62e917866c8f82f2ec1c16b077acf474e214
realFK98 <wishira2013.coth@gmail.com>
2024-09-16 18:14:07 +0700

    fix bug blur

--- src/components/CreateBoardModal.vue
+++ src/components/CreateBoardModal.vue
@@ -9,7 +9,7 @@ import { useBoardStore } from "../stores/useBoardStore.js";
 const boardStore = useBoardStore();
 const userStore = useUserStore();
 const newBoard = ref({
-  name: userStore.getUser().name + ' ' +'personal board',
+  name: userStore.getUser().name + " " + "personal board",
   ownerOid: userStore.getUser().oid,
 });
 const errorMessage = ref();
@@ -34,47 +34,49 @@ async function onSubmit() {
 </script>
 
 <template>
-  <div
-    class="fixed flex w-screen flex-col items-center justify-center rounded-lg"
-  >
-    <div class="flex h-max w-[27rem] flex-col rounded-md bg-neutral p-6">
-      <div class="text-2xl font-bold text-slate-300">New Board</div>
-      <div class="divider"></div>
-      <div class="itbkk-message flex flex-col gap-2 text-slate-300">
-        <div class="">Name</div>
-        <div>
-          <input
-            v-model="newBoard.name"
-            maxlength="120"
-            type="text"
-            placeholder="Your board name"
-            class="input input-bordered h-11 w-full bg-secondary"
-          />
+  <Teleport to="body">
+    <div
+      class="fixed right-0 top-0 z-[1000] flex h-screen w-screen flex-col items-center justify-center rounded-lg backdrop-blur-sm transition-all duration-500"
+    >
+      <div class="flex h-max w-[27rem] flex-col rounded-md bg-neutral p-6">
+        <div class="text-2xl font-bold text-slate-300">New Board</div>
+        <div class="divider"></div>
+        <div class="itbkk-message flex flex-col gap-2 text-slate-300">
+          <div class="">Name</div>
+          <div>
+            <input
+              v-model="newBoard.name"
+              maxlength="120"
+              type="text"
+              placeholder="Your board name"
+              class="input input-bordered h-11 w-full bg-secondary"
+            />
+          </div>
+          <div class="text-error">
+            {{ errorMessage }}
+          </div>
         </div>
-        <div class="text-error">
-          {{ errorMessage }}
+        <div class="divider"></div>
+        <div class="mt-4 flex justify-end gap-3">
+          <form method="dialog">
+            <Button
+              class="itbkk-button-confirm btn-success text-slate-200"
+              message="Save"
+              @click="onSubmit"
+              :disabled="newBoard.name.length === 0"
+            />
+          </form>
+          <form method="dialog">
+            <Button
+              class="itbkk-button-cancel text-slate-200"
+              message="Cancel"
+              @click="$router.push({ name: 'Boards' })"
+            />
+          </form>
         </div>
-      </div>
-      <div class="divider"></div>
-      <div class="mt-4 flex justify-end gap-3">
-        <form method="dialog">
-          <Button
-            class="itbkk-button-confirm btn-success text-slate-200"
-            message="Save"
-            @click="onSubmit"
-            :disabled="newBoard.name.length === 0"
-          />
-        </form>
-        <form method="dialog">
-          <Button
-            class="itbkk-button-cancel text-slate-200"
-            message="Cancel"
-            @click="$router.push({ name: 'Boards' })"
-          />
-        </form>
       </div>
     </div>
-  </div>
+  </Teleport>
 </template>
 
 <style scoped></style>
