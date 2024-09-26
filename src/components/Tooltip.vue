c0c9ba3895aa8a36f759bea262c6653ad020d6a9
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-09-26 20:40:32 +0700

    fig-bug-tooltip-complete

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,18 +1,39 @@
 <script setup>
+import { onMounted } from 'vue';
 import { ref } from 'vue';
+import { useUserStore } from '@/stores/useUserStore';
+import {getVisibilityByOid} from '../services/apiVisibility'
+import { useRoute } from 'vue-router';
 
+const route = useRoute()
 
+const userStore = useUserStore()
 const isTooltipVisible = ref(false);
+const isOwnerBoard = ref()
+
+onMounted(async () => {
+    const visibilityLoad = await getVisibilityByOid(route.params.oid);
+    if(userStore.getUser().oid === visibilityLoad.data.owner.oid){
+        isOwnerBoard.value = true
+  } else{
+    isOwnerBoard.value = false
+  }
+})
 
 // Show tooltip on hover
 const showTooltip = () => {
-  isTooltipVisible.value = true;
+    if(isOwnerBoard.value){
+        isTooltipVisible.value = false;
+    } else {
+        isTooltipVisible.value = true;
+    }
 };
 
 // Hide tooltip when not hovering
 const hideTooltip = () => {
   isTooltipVisible.value = false;
 };
+
 </script>
  
 <template>
@@ -20,9 +41,6 @@ const hideTooltip = () => {
     <div @mouseover="showTooltip" @mouseleave="hideTooltip">
         <slot></slot>
     </div>
-    <!-- <button @mouseover="showTooltip" @mouseleave="hideTooltip">
-      Hover over me
-    </button> -->
     <div v-if="isTooltipVisible" class="tooltip-content">
       You need to be board owner to perform this action.
     </div>
