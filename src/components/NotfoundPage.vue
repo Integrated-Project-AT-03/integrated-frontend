400cdd7876b803fa1c60bcc657ba6af32611b990
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-09-27 12:33:23 +0700

    fix-notfound-again

--- src/components/NotfoundPage.vue
+++ src/components/NotfoundPage.vue
@@ -6,13 +6,13 @@ const router = useRouter();
  
 <template>
 <div class="w-full h-screen flex justify-center items-center">
-    <div class="flex flex-col">
+    <div class="flex flex-col mt-[-7rem]">
         <img src="../../public/images/404-animation.gif" width="500" height="500" alt="Notfound-animation">
-        <div class="flex flex-col absolute bottom-52 right-[25.5rem] items-center font-bold gap-2 font-serif">
+        <div class="flex flex-col absolute top-[30rem] right-[26rem] items-center font-bold gap-2 font-serif">
             <div class="text-3xl">Page not Found</div>
             <div>Seems like you have lost your way. Let's bring you back home to
                 <span class="text-[#CE50B7] hover:underline cursor-pointer" 
-                @click="router.push({name: 'Boards'})"> Go home</span>
+                @click="router.push({name: 'Boards'})">Boards</span>
             </div>
         </div>
     </div>
