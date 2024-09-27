561ae8f6ec3a6b2f96617ea4146793116c6ea874
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-09-27 12:21:00 +0700

    make-notfound-page

--- src/components/NotfoundPage.vue
+++ src/components/NotfoundPage.vue
@@ -1,10 +1,21 @@
 <script setup>
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 </script>
  
 <template>
-<div class="w-full h-screen">
-    <div>404na</div>
+<div class="w-full h-screen flex justify-center items-center">
+    <div class="flex flex-col">
+        <img src="../../public/images/404-animation.gif" width="500" height="500" alt="Notfound-animation">
+        <div class="flex flex-col absolute bottom-52 items-center font-bold gap-2 font-serif">
+            <div class="text-3xl">Page not Found</div>
+            <div>Seems like you have lost your way. Let's bring you back home to
+                <span class="text-[#CE50B7] hover:underline cursor-pointer" 
+                @click="router.push({name: 'Boards'})"> Go home</span>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
  
