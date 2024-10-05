963dc7896e6634b4b05fde49072a7aeffc713928
realFK98 <wishira2013.coth@gmail.com>
2024-10-05 19:07:50 +0700

    add simple row

--- src/views/CollaboratorManagerPage.vue
+++ src/views/CollaboratorManagerPage.vue
@@ -1,20 +1,20 @@
 <script setup>
-import Button from '@/components/Button.vue';
-import { useRouter } from 'vue-router';
-import AddCollaboratorModal from '../components/AddCollaboratorModal.vue'
+import Button from "@/components/Button.vue";
+import { useRouter } from "vue-router";
+import AddCollaboratorModal from "../components/AddCollaboratorModal.vue";
 const router = useRouter();
 </script>
- 
+
 <template>
-<div class="flex flex-col w-full gap-3">
+  <div class="flex w-full flex-col gap-3">
     <AddCollaboratorModal />
     <div class="flex justify-end">
-        <Button
-              class="itbkk-collaborato-add"
-              bgcolor="#666666"
-              message="Add collaborator"
-              onclick="addcollaborator.showModal()"
-        />
+      <Button
+        class="itbkk-collaborato-add"
+        bgcolor="#666666"
+        message="Add collaborator"
+        onclick="addcollaborator.showModal()"
+      />
     </div>
     <table class="block max-h-[500px] divide-y divide-gray-200 overflow-scroll">
       <tbody class="divide-y divide-gray-300 bg-slate-100">
@@ -41,7 +41,7 @@ const router = useRouter();
             scope="col"
             class="px-6 py-3 text-center text-sm font-bold uppercase tracking-wider text-base-100"
           >
-            Access 
+            Access
           </th>
           <th
             scope="col"
@@ -50,14 +50,23 @@ const router = useRouter();
             Action
           </th>
         </tr>
-        <tr>
-
+        <tr class="itbkk-item itbkk-button-action hover:bg-slate-200">
+          <td class="px-6 py-4">
+            <div class="text-gray-900">{{ 1 }}</div>
+          </td>
+          <td class="whitespace-nowrap px-6 py-4">
+            <div class="itbkk-title text-sm text-gray-900">5555</div>
+          </td>
+          <td class="w-full whitespace-nowrap px-6 py-4">
+            <div class="itbkk-assignees w- text-sm text-gray-900">5</div>
+          </td>
+          <td class="whitespace-nowrap px-6 py-4">55</td>
+          <td class="whitespace-nowrap px-6 py-4">55</td>
         </tr>
+        <tr></tr>
       </tbody>
     </table>
-</div>
+  </div>
 </template>
- 
-<style scoped>
 
-</style>
+<style scoped></style>
