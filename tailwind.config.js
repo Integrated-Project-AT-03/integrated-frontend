9c0af04c98f6a91db475f4c6b1c42c03f5feb5e2
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-11-03 20:11:50 +0700

    fix-bug-daisy-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
@@ -25,5 +27,5 @@ export default {
       },
     ],
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
