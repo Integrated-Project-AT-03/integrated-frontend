c62d569a1a68781e6fe682f9c59285a6e290d5f2
realFK98 <wishira2013.coth@gmail.com>
2024-11-10 20:57:27 +0700

    fix invite modal

--- src/components/CollabModal.vue
+++ src/components/CollabModal.vue
@@ -21,8 +21,6 @@ const props = defineProps({
 });
 
 const emits = defineEmits(["message"]);
-const collabStore = useCollabStore();
-const route = useRoute();
 </script>
 
 <template>
--- src/views/InvitePage.vue
+++ src/views/InvitePage.vue
@@ -10,7 +10,7 @@ const inviterName = ref("");
 const boardName = ref("");
 const accessRight = ref("");
 const errorMessage = ref("");
-
+const emits = defineEmits(["message, loading"]);
 async function acceptInvite() {
   errorMessage.value = "";
   try {
@@ -61,47 +61,53 @@ onMounted(async () => {
   } catch (error) {
     console.error("Error loading invitation details:", error);
     errorMessage.value = "An error occurred while loading invitation details.";
+  } finally {
+    emits("loading", false);
   }
 });
 </script>
 
 <template>
-  <div class="flex h-screen w-full items-center justify-center">
+  <Teleport to="body">
     <div
-      class="flex w-[28rem] flex-col items-center rounded-2xl bg-gray-900 p-10 text-white shadow-xl"
+      class="fixed top-0 z-[10000] flex h-screen w-screen items-center justify-center backdrop-blur-sm"
     >
-      <h1
-        class="mb-6 bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-2xl font-extrabold text-transparent"
+      <div
+        class="flex w-[28rem] flex-col items-center rounded-2xl bg-gray-900 p-10 text-white shadow-xl"
       >
-        Invitation to Collaboration
-      </h1>
-
-      <p class="mb-6 text-center text-lg leading-relaxed text-gray-300">
-        <span class="font-semibold text-white">{{ inviterName }}</span> has
-        invited you to collaborate with
-        <span class="font-semibold text-white">{{ accessRight }}</span> access
-        rights on the
-        <span class="font-semibold text-white">{{ boardName }}</span> board.
-      </p>
-
-      <div v-if="errorMessage" class="mb-6 text-center text-red-500">
-        {{ errorMessage }}
-      </div>
-
-      <div class="flex gap-4">
-        <button
-          @click="acceptInvite"
-          class="transform rounded-lg bg-green-500 px-6 py-2 font-semibold text-white shadow-md transition duration-300 ease-in-out hover:scale-105 hover:bg-green-600"
+        <h1
+          class="mb-6 bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text text-2xl font-extrabold text-transparent"
         >
-          Accept Invitation
-        </button>
-        <button
-          @click="declineInvite"
-          class="transform rounded-lg bg-red-500 px-6 py-2 font-semibold text-white shadow-md transition duration-300 ease-in-out hover:scale-105 hover:bg-red-600"
-        >
-          Decline
-        </button>
+          Invitation to Collaboration
+        </h1>
+
+        <p class="mb-6 text-center text-lg leading-relaxed text-gray-300">
+          <span class="font-semibold text-white">{{ inviterName }}</span> has
+          invited you to collaborate with
+          <span class="font-semibold text-white">{{ accessRight }}</span> access
+          rights on the
+          <span class="font-semibold text-white">{{ boardName }}</span> board.
+        </p>
+
+        <div v-if="errorMessage" class="mb-6 text-center text-red-500">
+          {{ errorMessage }}
+        </div>
+
+        <div class="flex gap-4">
+          <button
+            @click="acceptInvite"
+            class="transform rounded-lg bg-green-500 px-6 py-2 font-semibold text-white shadow-md transition duration-300 ease-in-out hover:scale-105 hover:bg-green-600"
+          >
+            Accept Invitation
+          </button>
+          <button
+            @click="declineInvite"
+            class="transform rounded-lg bg-red-500 px-6 py-2 font-semibold text-white shadow-md transition duration-300 ease-in-out hover:scale-105 hover:bg-red-600"
+          >
+            Decline
+          </button>
+        </div>
       </div>
     </div>
-  </div>
+  </Teleport>
 </template>
