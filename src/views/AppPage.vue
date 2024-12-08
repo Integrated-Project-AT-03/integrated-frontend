e177edca7896bdd5af4cafd7340dece535bb99ee
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-12-08 14:44:12 +0700

    collab-page-res-success

--- src/views/AppPage.vue
+++ src/views/AppPage.vue
@@ -70,7 +70,7 @@ const handleLoading = (loadingStatus) => {
 
       <div
         v-show="$route.name == 'Collab'"
-        class="itbkk-board-name flex justify-center text-3xl font-bold"
+        class="itbkk-board-name flex justify-center mt-24 text-3xl font-bold"
       >
         Collaborator Management
       </div>
